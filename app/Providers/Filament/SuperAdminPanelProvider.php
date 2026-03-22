6eaaea25a5227803bb172572b7b1815610938a37
JOSGRAN <soysalas279@gmail.com>
2026-03-21 21:50:50 -0500

    feat(ui): polish premium theme (modal, dropdown, badges)

--- app/Providers/Filament/SuperAdminPanelProvider.php
+++ app/Providers/Filament/SuperAdminPanelProvider.php
@@ -18,16 +18,16 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
 use Filament\Support\Assets\Css;
+use Filament\Support\Colors\Color;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
-use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Illuminate\Support\Facades\Vite;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class SuperAdminPanelProvider extends PanelProvider
 {
